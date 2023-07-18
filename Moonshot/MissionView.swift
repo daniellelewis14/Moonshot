ac17dc3bf00bec33df1a745c7e0728eadfdd8f6e
daniellelewis14 <yurts.third_0v@icloud.com>
2023-07-18 19:32:52 -0400

    Create initializer, attach crew members to MissionView

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -10,6 +10,25 @@ import SwiftUI
 struct MissionView: View {
     let mission: Mission
     
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
+    let crew: [CrewMember]
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missions \(member.name)")
+            }
+        }
+    }
+    
     var body: some View {
         GeometryReader { geometry in
             ScrollView {
@@ -26,6 +45,35 @@ struct MissionView: View {
                             .padding(.bottom, 5)
                         
                         Text(mission.description)
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack {
+                                ForEach(crew, id: \.role) { crewMember in
+                                    NavigationLink {
+                                        Text("Astronaut details")
+                                    } label: {
+                                        HStack {
+                                            Image(crewMember.astronaut.id)
+                                                .resizable()
+                                                .frame(width: 104, height: 72)
+                                                .clipShape(Capsule())
+                                                .overlay(
+                                                    Capsule()
+                                                        .strokeBorder(.white, lineWidth: 1)
+                                                )
+                                            VStack(alignment: .leading) {
+                                                Text(crewMember.astronaut.name)
+                                                    .foregroundColor(.white)
+                                                    .font(.headline)
+                                                Text(crewMember.role)
+                                                    .foregroundColor(.secondary)
+                                            }
+                                        }
+                                        .padding(.horizontal)
+                                    }
+                                }
+                            }
+                        }
                     }
                     .padding(.horizontal)
                 }
@@ -40,8 +88,11 @@ struct MissionView: View {
 
 struct MissionView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    
     static var previews: some View {
-        MissionView(mission: missions[0])
+        MissionView(mission: missions[0], astronauts: astronauts)
             .preferredColorScheme(.dark)
     }
 }
