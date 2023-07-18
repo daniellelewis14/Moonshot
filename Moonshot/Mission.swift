06bbb88cb352aef7b5dd0bb987b8efe5c8971069
daniellelewis14 <yurts.third_0v@icloud.com>
2023-07-18 17:53:12 -0400

    Add Mission struct that conforms to Codable

---
+++ Moonshot/Mission.swift
@@ -0,0 +1,22 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Danielle Lewis on 7/18/23.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
+
+
