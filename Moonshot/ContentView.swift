e2ca4dbabfb4a3f2c49ab0a7f45b5ce10ba121ec
daniellelewis14 <yurts.third_0v@icloud.com>
2023-07-18 18:14:20 -0400

    Initial layout of assets and decoded JSON items

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -12,9 +12,38 @@ struct ContentView: View {
     
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
-    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+        
     var body: some View {
-        Text("\(astronauts.count)")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    Text(mission.launchDate ?? "N/A")
+                                        .font(.caption)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
+        }
     }
 }
 
